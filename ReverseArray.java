4768056c4fd88beba1a942dca9eeb47ac48b91cb
ShivaniR-gh <shivani53286@gmail.com>
2026-03-22 22:15:02 +0530

    Initial commit

---
+++ ReverseArray.java
@@ -0,0 +1,25 @@
+class ReverseArray {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            // swap
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        // print result
+        System.out.println("Reversed Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
